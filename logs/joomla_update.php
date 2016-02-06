614737d0c93e41d59d188c94937b577b2b2813da
Farid Rn <faridv@gmail.com>
2016-02-06 19:12:19 +0330

    Updated Joomla

---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-01-08 15:20:58 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-01-08T15:20:58+00:00	INFO ::1	update	Update started by user Super User (895). Old version is 3.4.3.
+2016-01-08T15:20:59+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.8/Joomla_3.4.x_to_3.4.8-Stable-Patch_Package.zip.
+2016-01-08T15:21:06+00:00	INFO ::1	update	File Joomla_3.4.x_to_3.4.8-Stable-Patch_Package.zip successfully downloaded.
+2016-01-08T15:21:06+00:00	INFO ::1	update	Starting installation of new version.
+2016-01-08T15:21:14+00:00	INFO ::1	update	Finalising installation.
+2016-01-08T15:21:15+00:00	INFO ::1	update	Deleting removed files and folders.
+2016-01-08T15:21:20+00:00	INFO ::1	update	Cleaning up after installation.
+2016-01-08T15:21:20+00:00	INFO ::1	update	Update to version 3.4.8 is complete.
